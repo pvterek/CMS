ac3e797b6a68c5bf6fb8b3999b1832108bc386f5
pvterek <125750554+pvterek@users.noreply.github.com>
2024-03-08 18:25:39 +0100

    Added calendar option in the Home Index view

--- CMS/Data/ApplicationDbContext.cs
+++ CMS/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using CMS.Models;
+using CMS.ViewModels;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +14,19 @@ namespace CMS.Data
         public DbSet<Visit> Visit { get; set; } = default!;
         public DbSet<Patient> Patient { get; set; } = default!;
         public DbSet<Employee> Employee { get; set; } = default!;
+
+        public async Task<List<VisitViewModel>> GetVisitsForDateRange(DateTime startDate, DateTime endDate)
+        {
+            return await Visit
+                .Where(p => p.VisitTime >= startDate && p.VisitTime < endDate)
+                .Join(Patient, visit => visit.PatientId, patient => patient.PatientId, (visit, patient) => new { visit, patient })
+                .Join(Employee, vp => vp.visit.EmployeeId, employee => employee.EmployeeId, (vp, employee) => new VisitViewModel
+                {
+                    Visit = vp.visit,
+                    Patient = vp.patient,
+                    Employee = employee
+                })
+                .ToListAsync();
+        }
     }
 }
